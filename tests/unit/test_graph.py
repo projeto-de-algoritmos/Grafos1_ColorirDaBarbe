b815c3bc91f78ad4766d3d04d5835c0e400abfa0
giovannabbottino <giovannabbottino@gmail.com>
2022-11-14 19:36:14 -0300

    + dfs test
    Co-authored-by: Felipe Agustini <felipeboccardi@hotmail.com>

--- tests/unit/test_graph.py
+++ tests/unit/test_graph.py
@@ -1,9 +1,11 @@
-from src.model.graph import Graph
+import sys 
+sys.path.append("src/") 
 
+from controller.graph import GraphController
 import unittest
 import numpy as np
 
-class TestFloodFill(unittest.TestCase):
+class TestFloodBFS(unittest.TestCase):
 
     def test_blank_image(self):
         image = np.array([
@@ -11,14 +13,14 @@ class TestFloodFill(unittest.TestCase):
             [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], 
             [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], 
             [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]])
-        graph = Graph(image)
+        graph = GraphController(image)
         expected = np.array([
             [[1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1]], 
             [[1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1]],  
             [[1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1]],  
             [[1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1]]])
         actual = graph.floodFill(image, 0, 0, (1, 1, 1, 1))
-        self.assertEqual(actual.all(), expected.all())
+        self.assertEqual(np.array_equal(actual, expected), True)
 
     def test_limited_pixel(self):
         image = np.array([
@@ -26,14 +28,14 @@ class TestFloodFill(unittest.TestCase):
             [[1, 1, 1], [1, 1, 1], [0, 0, 0], [0, 0, 0]], 
             [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], 
             [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]])
-        graph = Graph(image)
+        graph = GraphController(image)
         expected = np.array([
             [[1, 1, 1], [1, 1, 1], [0, 0, 0], [0, 0, 0]], 
             [[1, 1, 1], [1, 1, 1], [0, 0, 0], [0, 0, 0]], 
             [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], 
             [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]])
         actual = graph.floodFill(image, 0, 0, (1, 1, 1, 1))    
-        self.assertEqual(actual.all(), expected.all())
+        self.assertEqual(np.array_equal(actual, expected), True)
 
     def test_limited_pixel_open_border(self):
         image = np.array([
@@ -41,14 +43,14 @@ class TestFloodFill(unittest.TestCase):
             [[1, 1, 1], [0, 0, 0], [0, 0, 0], [0, 0, 0]], 
             [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], 
             [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]])
-        graph = Graph(image)
+        graph = GraphController(image)
         expected = np.array([
             [[1, 1, 1], [1, 1, 1], [0, 0, 0], [0, 0, 0]], 
             [[1, 1, 1], [0, 0, 0], [0, 0, 0], [0, 0, 0]], 
             [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], 
             [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]])
         actual = graph.floodFill(image, 0, 0, (1, 1, 1, 1))
-        self.assertEqual(actual.all(), expected.all())
+        self.assertEqual(np.array_equal(actual, expected), True)
 
     def test_multiple_colors(self):
         image = np.array([
@@ -56,14 +58,61 @@ class TestFloodFill(unittest.TestCase):
             [[1, 1, 1], [2, 2, 2], [0, 0, 0], [0, 0, 0]], 
             [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], 
             [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]])
-        graph = Graph(image)
+        graph = GraphController(image)
         expected = np.array([
             [[0, 0, 0], [1, 1, 1], [0, 0, 0], [0, 0, 0]], 
             [[1, 1, 1], [1, 1, 1], [0, 0, 0], [0, 0, 0]], 
             [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], 
             [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]])
         actual = graph.floodFill(image, 1, 1, (1, 1, 1, 1))
-        self.assertEqual(actual.all(), expected.all())
+        self.assertEqual(np.array_equal(actual, expected), True)
+
+class TestFloodDFS(unittest.TestCase):
+    def test_limited_pixel(self):
+        image = np.array([
+            [[0, 0, 0], [1, 1, 1], [0, 0, 0], [0, 0, 0]], 
+            [[1, 1, 1], [1, 1, 1], [0, 0, 0], [0, 0, 0]], 
+            [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], 
+            [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]])
+        graph = GraphController(image, 'dfs')
+        expected = np.array([
+            [[1, 1, 1], [1, 1, 1], [0, 0, 0], [0, 0, 0]], 
+            [[1, 1, 1], [1, 1, 1], [0, 0, 0], [0, 0, 0]], 
+            [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], 
+            [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]])
+        actual = graph.floodFill(image, 0, 0, (1, 1, 1, 1)) 
+        print(actual)
+        self.assertEqual(np.array_equal(actual, expected), True)
+
+    def test_limited_pixel_open_border(self):
+        image = np.array([
+            [[0, 0, 0], [1, 1, 1], [0, 0, 0], [0, 0, 0]], 
+            [[1, 1, 1], [0, 0, 0], [0, 0, 0], [0, 0, 0]], 
+            [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], 
+            [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]])
+        graph = GraphController(image, 'dfs')
+        expected = np.array([
+            [[1, 1, 1], [1, 1, 1], [0, 0, 0], [0, 0, 0]], 
+            [[1, 1, 1], [0, 0, 0], [0, 0, 0], [0, 0, 0]], 
+            [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], 
+            [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]])
+        actual = graph.floodFill(image, 0, 0, (1, 1, 1, 1))
+        self.assertEqual(np.array_equal(actual, expected), True)
+
+    def test_multiple_colors(self):
+        image = np.array([
+            [[0, 0, 0], [1, 1, 1], [0, 0, 0], [0, 0, 0]], 
+            [[1, 1, 1], [2, 2, 2], [0, 0, 0], [0, 0, 0]], 
+            [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], 
+            [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]])
+        graph = GraphController(image, 'dfs')
+        expected = np.array([
+            [[0, 0, 0], [1, 1, 1], [0, 0, 0], [0, 0, 0]], 
+            [[1, 1, 1], [1, 1, 1], [0, 0, 0], [0, 0, 0]], 
+            [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], 
+            [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]])
+        actual = graph.floodFill(image, 1, 1, (1, 1, 1, 1))
+        self.assertEqual(np.array_equal(actual, expected), True)
 
 if __name__ == '__main__':
     unittest.main()
